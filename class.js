c5d2135110ae804089cb74a1b50aed153a05f534
Minto108 <158478797+Minto108@users.noreply.github.com>
2025-02-23 21:47:17 +0530

    Add files via upload

---
+++ class.js
@@ -0,0 +1,10 @@
+class car{
+    constructor(name,year){
+        this.name = name;
+        this.year = year;
+    }
+}
+
+let myCar = new car("Ford", 2024);
+console.log(myCar.name);
+console.log(myCar.year);
